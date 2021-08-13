a54aa3aed5f15eb9be526846e0c83388597758be
stephkri <stephane.krims@hotmail.ca>
2021-08-13 23:16:21 +0000

    Basic spinner file

---
+++ spinner1.js
@@ -0,0 +1,18 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+setTimeout(() => {
+  process.stdout.write('\r\\   ');
+}, 700);
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 900);
+setTimeout(() => {
+  process.stdout.write('\n');
+}, 1100);
